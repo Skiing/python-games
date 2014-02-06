eed642a4fc9cc81a9f36703893e43fe17adb6f45
Skiing <sam@pipebaum.com>
2014-02-05 18:04:26 -0600

    Initial Commit

---
+++ hello pyton 2.py
@@ -0,0 +1,6 @@
+print ("hello world!")
+print("what is your name?")
+myname = input ()
+print ("It is good to meet you, " + myname
+       ("It is good to meet you, " + myname)
+       
---
+++ hello pyton.py
@@ -0,0 +1,44 @@
+Python 3.3.3 (v3.3.3:c3896275c0f6, Nov 18 2013, 21:18:40) [MSC v.1600 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> hello + world
+Traceback (most recent call last):
+  File "<pyshell#0>", line 1, in <module>
+    hello + world
+NameError: name 'hello' is not defined
+>>> "hello" + "world"
+'helloworld'
+>>> mom = hi mom
+SyntaxError: invalid syntax
+>>> 
+>>> 
+>>> spam = hi mom
+SyntaxError: invalid syntax
+>>> mom = "hi mom "
+>>> mom
+'hi mom '
+>>> print ("it is nice meet you sam)
+       
+SyntaxError: EOL while scanning string literal
+>>> print
+<built-in function print>
+>>> sam = input
+>>> print ('it is nice meet you, " + sam
+       
+SyntaxError: EOL while scanning string literal
+>>> print ('it is nice meet you, " + sam)
+       
+SyntaxError: EOL while scanning string literal
+>>> print ("hello world ! ")
+hello world ! 
+>>> print ("what is your name ? ")
+what is your name ? 
+>>> mom
+'hi mom '
+>>> mom
+'hi mom '
+>>> Sam = input
+>>> print ("It is good to meet you, " + Sam
+       print
+       
+SyntaxError: invalid syntax
+>>> 
